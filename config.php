45bf23e660f28051e5de1bbb7a90396a502a2249
selosele <osh3384@naver.com>
2021-04-04 15:27:10 +0900

    branch 생성용 최초 커밋

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Jigsaw theme Destiny',
+    'description' => 'Website description.',
+    'collections' => [],
+];
